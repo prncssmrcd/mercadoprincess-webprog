84cc2417be7a26e21be528c6e5f972be311994bf
prncssmrcd <123874259+prncssmrcd@users.noreply.github.com>
2026-05-05 22:19:30 +0800

    Delete aquino-client directory

--- aquino-client/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite"; // Add this [cite: 106]
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-});
